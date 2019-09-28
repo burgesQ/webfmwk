a4732cdb22d1d859219cb3c55bd08ee04d4e453f
Quentin Burgess <qutn.burgess@gmail.com>
2019-09-29 00:56:01 +0200

    update example from godoc

--- doc.go
+++ doc.go
@@ -3,28 +3,31 @@ Package webfmwk implements an minimalist Go web framework
 
 Example:
   package main
-import (
-	w "github.com/burgesQ/webfmwk/v2"
-)
-// Handler
-func hello(c w.IContext) error {
-	return c.JSONOk("Hello, World!")
-}
-func main() {
-	// Echo instance
-	s := w.InitServer(true)
 
-	// Routes
-	s.GET("/hello", hello)
+  import (
+	  w "github.com/burgesQ/webfmwk/v2"
+  )
+  // Handler
+  func hello(c w.IContext) error {
+	  return c.JSONOk("Hello, World!")
+  }
 
-  // start server on :4242
-	go func() {
-		s.Start(":4242")
-	}()
+  func main() {
+	  // Echo instance
+	  s := w.InitServer(true)
 
-	// ctrl+c is handled internaly
-	defer s.WaitAndStop()
-}
+  	// Routes
+	  s.GET("/hello", hello)
+
+    // start server on :4242
+	  go func() {
+		  s.Start(":4242")
+	  }()
+
+  	// ctrl+c is handled internaly
+	  defer s.WaitAndStop()
+  }
+
 Learn more at https://github.com/burgesQ/echo
 */
 package webfmwk
