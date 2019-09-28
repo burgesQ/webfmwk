b4545b2219bf5f3ea8b760819f5e406bafad301a
Quentin Burgess <qutn.burgess@gmail.com>
2019-09-28 23:45:47 +0200

    update doc

--- doc.go
+++ doc.go
@@ -1,3 +1,30 @@
-// webfmwk is a basic API web framework
-// TODO: blablabla
+/*
+Package webfmwk implements an minimalist Go web framework
+
+Example:
+  package main
+import (
+	w "github.com/burgesQ/webfmwk/v2"
+)
+// Handler
+func hello(c w.IContext) error {
+	return c.JSONOk("Hello, World!")
+}
+func main() {
+	// Echo instance
+	s := w.InitServer(true)
+
+	// Routes
+	s.GET("/hello", hello)
+
+  // start server on :4242
+	go func() {
+		s.Start(":4242")
+	}()
+
+	// ctrl+c is handled internaly
+	defer s.WaitAndStop()
+}
+Learn more at https://github.com/burgesQ/echo
+*/
 package webfmwk
