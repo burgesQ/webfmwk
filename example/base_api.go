cba5da4572c5fd89fbfdd3e232419cd0db13ff8d
Quentin Burgess <qutn.burgess@gmail.com>
2019-09-29 16:08:02 +0200

    rename test directory to example

--- example/base_api.go
+++ example/base_api.go
@@ -2,17 +2,13 @@ package main
 
 import (
 	w "github.com/burgesQ/webfmwk"
-	"github.com/burgesQ/webfmwk/log"
 )
 
 func main() {
-
-	// init logging
-	log.SetLogLevel(log.LOG_DEBUG)
-	log.Init(log.LOGGER_STDOUT | log.LOGFORMAT_LONG)
-
 	// init server w/ ctrl+c support
 	s := w.InitServer(true)
+
+	s.SetPrefix("/api")
 
 	s.GET("/test", func(c w.IContext) error {
 		return c.JSONOk("ok")
@@ -20,11 +16,7 @@ func main() {
 
 	// start asynchronously on :4242
 	go func() {
-		s.StartTLS(":4242", TLSConfig{
-			Cert:     "/path/to/cert",
-			Key:      "/path/to/key",
-			Insecure: true,
-		})
+		s.Start(":4242")
 	}()
 
 	// ctrl+c is handled internaly
---
+++ example/tls.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	w "github.com/burgesQ/webfmwk"
+)
+
+func main() {
+	// init server w/ ctrl+c support
+	s := w.InitServer(true)
+
+	s.GET("/test", func(c w.IContext) error {
+		return c.JSONOk("ok")
+	})
+
+	// start asynchronously on :4242
+	go func() {
+		s.StartTLS(":4242", TLSConfig{
+			Cert:     "/path/to/cert",
+			Key:      "/path/to/key",
+			Insecure: true,
+		})
+	}()
+
+	// ctrl+c is handled internaly
+	defer s.WaitAndStop()
+}
