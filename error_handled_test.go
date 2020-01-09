7079369f5cd9b52e402ee6c57ecaf1411a868e42
Quentin Burgess <qutn.burgess@gmail.com>
2020-01-09 12:00:09 +0100

    error: implementent error wrapping

--- error_handled_test.go
+++ error_handled_test.go
@@ -1,6 +1,7 @@
 package webfmwk
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 
@@ -30,22 +31,63 @@ func TestFactory(t *testing.T) {
 	z.AssertEqual(t, _testContent, test.GetContent())
 }
 
+func TestWrapping(t *testing.T) {
+	var (
+		testE = errors.New("what a pretty test")
+		e     = NewUnauthorized(_testContent).SetWrapped(testE)
+		eh    IErrorHandled
+	)
+
+	t.Run("test error is", func(t *testing.T) {
+		if !errors.Is(e, testE) {
+			t.Errorf("ErrorHandled isn't a testE")
+		}
+	})
+
+	t.Run("test error as", func(t *testing.T) {
+		if !errors.As(e, &eh) {
+			t.Errorf("Unauthorized isn't an ErrorHandled")
+		}
+	})
+
+	t.Run("test error unwrap", func(t *testing.T) {
+		z.AssertEqual(t, e.Unwrap().Error(), testE.Error())
+	})
+
+	// test wrap
+
+	// test Is
+
+	// test As
+
+}
+
 func TestMethod(t *testing.T) {
 	var tests = map[string]struct {
 		actual, expected int
 	}{
+		"processing": {
+			NewProcessing(_testContent).GetOPCode(), http.StatusProcessing,
+		},
+
 		"no contet": {
 			NewNoContent().GetOPCode(), http.StatusNoContent,
 		},
+
 		"bad request": {
 			NewBadRequest(_testContent).GetOPCode(), http.StatusBadRequest,
+		},
+		"unauthorized": {
+			NewUnauthorized(_testContent).GetOPCode(), http.StatusUnauthorized,
 		},
 		"not found": {
 			NewNotFound(_testContent).GetOPCode(), http.StatusNotFound,
 		},
-
 		"not acceptable": {
 			NewNotAcceptable(_testContent).GetOPCode(), http.StatusNotAcceptable,
+		},
+		"conflict": {
+			NewConflict(_testContent).GetOPCode(), http.StatusConflict,
 		},
 		"unprocessable": {
 			NewUnprocessable(_testContent).GetOPCode(), http.StatusUnprocessableEntity,
