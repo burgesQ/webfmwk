0e2a08234a699569fc5cfa114ac2980d051f75e1
Quentin Burgess <qutn.burgess@gmail.com>
2020-03-03 10:05:43 +0100

    update default config

--- tls.go
+++ tls.go
@@ -15,6 +15,31 @@ type (
 	}
 )
 
+var (
+	DefaultCurve = []tls.CurveID{
+		tls.CurveP256,
+		tls.X25519,
+	}
+	DefaultCipher = []uint16{
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256, // HTTP/2-required AES_128_GCM_SHA256 cipher
+		tls.TLS_AES_128_GCM_SHA256,                // 1.3 tls cipher
+		tls.TLS_AES_256_GCM_SHA384,                // 1.3 tls cipher
+		tls.TLS_CHACHA20_POLY1305_SHA256,          // 1.3 tls cipher
+		// tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, // ECDHE-RSA-AES256-GCM-SHA384
+		// tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,    // ECDHE-RSA-AES256-SHA
+		// tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, // ECDH-RSA-AES256-GCM-SHA384
+		// tls.TLS_ECDHE_RSA_WITH_AES_256_SHA384,    // ECDHE-RSA-AES256-SHA384
+		// tls.TLS_ECDHE_RSA_WITH_AES_256_SHA,       // ECDH-RSA-AES256-SHA
+		// tls.TLS_ECDHE_RSA_WITH_AES_256_SHA384,    // ECDH-RSA-AES256-SHA384
+		// Tl's.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, // ECDHE-ECDSA-AES256-GCM-SHA384
+		// tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,    // ECDHE-ECDSA-AES256-SHA
+		// tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, // ECDH-ECDSA-AES256-GCM-SHA384
+		// tls.TLS_ECDHE_ECDSA_WITH_AES_256_SHA384,  // ECDHE-ECDSA-AES256-SHA384
+		// tls.TLS_ECDHE_ECDSA_WITH_AES_256_SHA,     // ECDH-ECDSA-AES256-SHA
+		// tls.TLS_ECDHE_ECDSA_WITH_AES_256_SHA384,  // ECDH-ECDSA-AES256-SHA384
+	}
+)
+
 // StartTLS expose an server to an HTTPS endpoint
 func (s *Server) StartTLS(addr string, tlsStuffs TLSConfig) {
 	s.internalHandler()
@@ -30,31 +55,10 @@ func (s *Server) loadTLS(worker *http.Server, tlsCfg TLSConfig) {
 		InsecureSkipVerify:       tlsCfg.Insecure,
 		Certificates:             make([]tls.Certificate, 1),
 		PreferServerCipherSuites: true,
-		CurvePreferences: []tls.CurveID{
-			tls.CurveP256,
-			tls.X25519,
-		},
-		MinVersion: tls.VersionTLS12,
-		MaxVersion: tls.VersionTLS13,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256, // HTTP/2-required AES_128_GCM_SHA256 cipher
-			tls.TLS_AES_128_GCM_SHA256,                // 1.3 tls cipher
-			tls.TLS_AES_256_GCM_SHA384,                // 1.3 tls cipher
-			tls.TLS_CHACHA20_POLY1305_SHA256,          // 1.3 tls cipher
-			// tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, // ECDHE-RSA-AES256-GCM-SHA384
-			// tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,    // ECDHE-RSA-AES256-SHA
-			// tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384, // ECDH-RSA-AES256-GCM-SHA384
-			// tls.TLS_ECDHE_RSA_WITH_AES_256_SHA384,    // ECDHE-RSA-AES256-SHA384
-			// tls.TLS_ECDHE_RSA_WITH_AES_256_SHA,       // ECDH-RSA-AES256-SHA
-			// tls.TLS_ECDHE_RSA_WITH_AES_256_SHA384,    // ECDH-RSA-AES256-SHA384
-			// Tl's.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, // ECDHE-ECDSA-AES256-GCM-SHA384
-			// tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,    // ECDHE-ECDSA-AES256-SHA
-			// tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, // ECDH-ECDSA-AES256-GCM-SHA384
-			// tls.TLS_ECDHE_ECDSA_WITH_AES_256_SHA384,  // ECDHE-ECDSA-AES256-SHA384
-			// tls.TLS_ECDHE_ECDSA_WITH_AES_256_SHA,     // ECDH-ECDSA-AES256-SHA
-			// tls.TLS_ECDHE_ECDSA_WITH_AES_256_SHA384,  // ECDH-ECDSA-AES256-SHA384
-
-		},
+		CurvePreferences:         DefaultCurve,
+		MinVersion:               tls.VersionTLS12,
+		MaxVersion:               tls.VersionTLS13,
+		CipherSuites:             DefaultCipher,
 	}
 
 	cert, err := tls.LoadX509KeyPair(tlsCfg.Cert, tlsCfg.Key)
