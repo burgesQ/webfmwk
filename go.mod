80d913606716d4f8f1228f2d6b53ae2aff1c58e9
Quentin Burgess <qutn.burgess@gmail.com>
2020-11-09 19:36:25 +0100

    add runner methos

--- go.mod
+++ go.mod
@@ -9,10 +9,12 @@ require (
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/gorilla/schema v1.1.0
-	github.com/kr/pretty v0.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 )
 
 go 1.13
--- runner.go
+++ runner.go
@@ -5,6 +5,13 @@ import (
 )
 
 type (
+	IAddress interface {
+		GetAddr() string
+		GetTLS() *TLSConfig
+		GetName() string
+		IsOk() bool
+	}
+
 	// Address hold the api server address
 	Address struct {
 		Addr string     `json:"addr"`
@@ -25,6 +32,18 @@ func (a Address) IsOk() bool {
 	return a.Addr != ""
 }
 
+func (a Address) GetAddr() string {
+	return a.Addr
+}
+
+func (a Address) GetTLS() *TLSConfig {
+	return a.TLS
+}
+
+func (a Address) GetName() string {
+	return a.Name
+}
+
 // Run allow to launch multiple server from a single call.
 // It take an vaarg Address param argument. WaitAndStop is called via defer.
 func (s *Server) Run(addrs ...Address) {
@@ -33,15 +52,15 @@ func (s *Server) Run(addrs ...Address) {
 	for i := range addrs {
 		addr := addrs[i]
 		if addr.IsOk() {
-			if addr.TLS != nil {
-				s.GetLogger().Infof("starting %s on https://%s", addr.Name, addr.Addr)
-				s.StartTLS(addr.Addr, addr.TLS)
+			if tls := addr.GetTLS(); tls != nil {
+				s.GetLogger().Infof("starting %s on https://%s", addr.GetName(), addr.GetAddr())
+				s.StartTLS(addr.GetAddr(), tls)
 			} else {
-				s.GetLogger().Infof("starting %s on http://%s", addr.Name, addr.Addr)
-				s.Start(addr.Addr)
+				s.GetLogger().Infof("starting %s on http://%s", addr.GetName(), addr.GetAddr())
+				s.Start(addr.GetAddr())
 			}
 		} else {
-			s.GetLogger().Errorf("invalid address format : %v", addr)
+			s.GetLogger().Errorf("invalid address format : %s", addr)
 		}
 	}
 }
