db298d127999bb900034a7e4e7fceccee86f209a
Quentin Burgess <qutn.burgess@gmail.com>
2023-12-05 17:18:18 +0100

    feat(handler): allow to specify the msg log level

--- handler/slogging/logging.go
+++ handler/slogging/logging.go
@@ -15,52 +15,64 @@ const (
 	_limitOutput    = 2048
 )
 
-// Handler generate an request ID and log information about
+var LogLevel = ((*slog.Logger).Info)
+
+// NewHandler generate an request ID and log information about
 // the newly receive request
 // The logger is then overloaded to add the request ID to every futur log message
-func Handler(next webfmwk.HandlerFunc) webfmwk.HandlerFunc {
-	return webfmwk.HandlerFunc(func(c webfmwk.Context) error {
-		var (
-			start = time.Now()
-			fc    = c.GetFastContext()
-			rid   = string(fc.Request.Header.Peek(HeaderRequestID))
-		)
+func NewHandler(method ...func(*slog.Logger, string, ...any)) webfmwk.Handler {
+	mh := ((*slog.Logger).Info)
 
-		if rid == "" {
-			rid = strconv.Itoa(int(c.GetFastContext().ID()))
-		}
-		c.SetHeader(HeaderRequestID, rid)
+	if len(method) > 0 {
+		mh = method[0]
+	}
 
-		lg := c.GetStructuredLogger().With(
-			slog.Group("request",
-				slog.String("id", rid),
-				slog.String("ip", webfmwk.GetIPFromRequest(fc)),
-				slog.String("method", string(fc.Method())),
-				slog.String("uri", string(fc.RequestURI()))))
+	return func(next webfmwk.HandlerFunc) webfmwk.HandlerFunc {
+		return webfmwk.HandlerFunc(func(c webfmwk.Context) error {
+			var (
+				start = time.Now()
+				fc    = c.GetFastContext()
+				rid   = string(fc.Request.Header.Peek(HeaderRequestID))
+			)
 
-		c.SetStructuredLogger(lg)
-
-		lg.Info("--> new request")
-
-		e := next(c)
-		elapsed := time.Since(start)
-		content := c.GetFastContext().Response.Body()
-		l := len(content)
-
-		if utf8.Valid(content) {
-			if l > _limitOutput {
-				lg.Debug("trunkated response",
-					slog.String("body", string(content[:_limitOutput])),
-					slog.Int("lim", _limitOutput))
-			} else {
-				lg.Debug("full response", slog.String("body", string(content)))
+			if rid == "" {
+				rid = strconv.Itoa(int(c.GetFastContext().ID()))
 			}
-		}
+			c.SetHeader(HeaderRequestID, rid)
 
-		lg.Info("<-- request done",
-			slog.Int("code", fc.Response.Header.StatusCode()),
-			slog.Any("elapsed", elapsed))
+			lg := c.GetStructuredLogger().With(
+				slog.Group("request",
+					slog.String("id", rid),
+					slog.String("ip", webfmwk.GetIPFromRequest(fc)),
+					slog.String("method", string(fc.Method())),
+					slog.String("uri", string(fc.RequestURI()))))
 
-		return e
-	})
+			c.SetStructuredLogger(lg)
+
+			(mh)(lg, "--> new request")
+
+			// LogLevel
+
+			e := next(c)
+			elapsed := time.Since(start)
+			content := c.GetFastContext().Response.Body()
+			l := len(content)
+
+			if utf8.Valid(content) {
+				if l > _limitOutput {
+					lg.Debug("trunkated response",
+						slog.String("body", string(content[:_limitOutput])),
+						slog.Int("lim", _limitOutput))
+				} else {
+					lg.Debug("full response", slog.String("body", string(content)))
+				}
+			}
+
+			(mh)(lg, "<-- request done",
+				slog.Int("code", fc.Response.Header.StatusCode()),
+				slog.Any("elapsed", elapsed))
+
+			return e
+		})
+	}
 }
